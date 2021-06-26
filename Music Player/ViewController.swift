8c9cc7d8c9865762e8a3279e1ae5c484966bed0f
LucasMPS <lucas.poncem@gmail.com>
2021-06-26 18:43:31 -0400

    Initial Commit

---
+++ Music Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music Player
+//
+//  Created by Lucas Ponce on 26-06-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
